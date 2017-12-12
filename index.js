d2fc52a334f9d5d3b8003360714e690060b3dcb2
Austin Albrecht <austin.aa@me.com>
2017-12-12 21:15:13 +0100

    Changed style to standardjs and made small tweaks

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
-var fs = require('fs');
+var path = require('path')
+var conf = require('./config.json')
 
-// External
-var chan = require('4chanjs');
-var Jimp = require("jimp");
-var async = require("async");
-require('console-stamp')(console, '[HH:MM:ss.l]');
+var chan = require('4chanjs')
+var Jimp = require('jimp')
+var async = require('async')
 
-var conf = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
+var board = chan.board(conf.board)
 
-var board = chan.board(conf.board);
-
-function cropAndSave(urlList) {
-  async.eachSeries(urlList, function(url, callback) {
+function cropAndSave (urlList) {
+  async.eachSeries(urlList, function (url, callback) {
     Jimp.read(url.url, function (err, image) {
-
       if (err == null) {
-        image.scaleToFit(512,512);
-        image.quality(80);
-        image.write(__dirname + "/tmp/" + url.tim + ".png", function () {
-          console.log("File saved");
-          callback();
-        });
+        image.scaleToFit(512, 512)
+        image.quality(80)
+
+        var filePath = path.join(__dirname, 'tmp', url.tim + '.png')
+
+        image.write(filePath, function () {
+          console.log('File saved')
+          callback()
+        })
       } else {
-        console.log(err);
-        callback("Failed to fetch image");
+        console.error('Failed to fetch image: ', url.url)
+        callback(err)
       }
-
-    });
-
-  }, function(err) {
-      if( err ) {
-        console.log('A file failed to download');
-        console.log(err);
-      } else {
-        console.log('All files have been downloaded successfully');
-      }
-  });
-
-
-
+    })
+  }, function (err) {
+    if (err) {
+      console.log('A file failed to download: ', err)
+    } else {
+      console.log('All files have been downloaded successfully')
+    }
+  })
 };
 
-
-board.catalog(function(err, pages){
-  console.log(err);
+board.catalog(function (err, pages) {
   if (pages == null) {
-    console.log("wat");
+    console.error('No pages were found: ', err)
   } else {
-
-    var urlList = [];
+    var urlList = []
 
     for (var i = 0; i < pages.length; i++) {
       for (var j = 0; j < pages[i].threads.length; j++) {
-
-        var url = board.image(pages[i].threads[j].tim + pages[i].threads[j].ext);
+        var url = board.image(pages[i].threads[j].tim + pages[i].threads[j].ext)
 
         switch (pages[i].threads[j].ext) {
-          case ".webm":
-          case ".gif":
-            console.log(pages[i].threads[j].tim + pages[i].threads[j].ext + " was not used because it's a webm or gif");
-            break;
+          case '.webm':
+          case '.gif':
+            console.info(pages[i].threads[j].tim + pages[i].threads[j].ext + " was not used because it's a webm or gif")
+            break
           default:
             if (pages[i].threads[j].tim > 0) {
-              var urlobj = {"url": url, "tim": pages[i].threads[j].tim};
-              urlList.push(urlobj);
+              var urlobj = {'url': url, 'tim': pages[i].threads[j].tim}
+              urlList.push(urlobj)
             }
         }
-
       }
     }
-    cropAndSave(urlList);
-
+    cropAndSave(urlList)
   }
-
-});
+})
